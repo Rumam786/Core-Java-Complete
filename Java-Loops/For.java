f980584086feca00831ab1c7356a9b21bafae915
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-07-25 07:10:11 -0700

    java-Loops

---
+++ Java-Loops/For.java
@@ -0,0 +1,7 @@
+class For {
+  public static void main(String[] args) {
+    for ( int i = 0; i < 11; i = i + 1) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ Java-Loops/each.java
@@ -0,0 +1,8 @@
+class A {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ferrari", "Mercedes"};
+    for ( String i : cars ) {
+      System.out.println(i);
+    }
+  }
+}

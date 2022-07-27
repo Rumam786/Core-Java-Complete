e49f2d5c718ab1bb795039b058e8b72e11b4cd9c
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-07-27 05:42:23 -0700

    java methods & functions

---
+++ Java-functions and Methods/factorialOfNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class D {
+	public static void printfactorial (int n) {
+        //for-loop
+        int Factorial = 1;
+        for (int i = n; i>=1; i--) {
+        Factorial = Factorial*i;
+        }
+        System.out.println(Factorial);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type Number");
+        int n = sc.nextInt();
+        System.out.println("Your Answer is");
+        printfactorial(n);
+	}
+}

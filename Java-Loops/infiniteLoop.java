dd55d8f6e39e097adc46b4c399e28da6769ffa91
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-07-27 07:33:35 -0700

    Create infiniteLoop.java

---
+++ Java-Loops/infiniteLoop.java
@@ -0,0 +1,10 @@
+class Loops { 
+ 
+  public static void main(String args[]) { 
+      int x = 100; 
+    while ( x>10 ) {
+      System.out.println("This is infinite Loop");
+      x++;
+    }
+  } 
+} 

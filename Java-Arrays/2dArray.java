67eeee0e86949ab4da68b595431a00d09076074d
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-07-30 03:30:37 -0700

    Create 2dArray.java

---
+++ Java-Arrays/2dArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class A {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int[][] marks = new int [2][3];
+		marks[0][0] = 10;
+		marks[0][1] = 20;
+		marks[0][2] = 30;
+		marks[1][0] = 40;
+		marks[1][1] = 50;
+		marks[1][2] = 60;
+		System.out.println(marks[0][0]);
+		System.out.println(marks[0][1]);
+		System.out.println(marks[0][2]);
+		System.out.println(marks[1][0]);
+		System.out.println(marks[1][1]);
+		System.out.println(marks[1][2]);
+	}
+}

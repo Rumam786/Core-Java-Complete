9c36fcf93f4bd31c0159c7eafc9da31ab44e19d0
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-08-02 03:43:47 -0700

    java

---
+++ Java-String/specialCharacter.java
@@ -0,0 +1,10 @@
+class A {
+	public static void main(String[] args) {
+		String name = "My Name is \"Ruman\" Ali";
+		String name1 = "My Name is \'Ruman\' Ali";
+		String name2 = "My Name is \\Ruman\\ Ali";
+		System.out.println(name);
+		System.out.println(name1);
+		System.out.println(name2);
+	}
+}

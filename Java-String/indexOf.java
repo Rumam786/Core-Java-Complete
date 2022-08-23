7f89dc58c474227eca7db0d41ca0b6d723d512bb
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-08-23 15:24:55 -0700

    java

--- Java-String/indexOf.java
+++ Java-String/indexOf.java
@@ -1,6 +1,6 @@
 class A {
 	public static void main(String[] args) {
 		String name = "My name is Ruman Ali";
-		System.out.println(name.indexOf("Ruman"));
+		System.out.println(name.indexOf("R"));
 	}
 }  // Output is 11.
--- Java-functions and Methods/factorialOfNumber.java
+++ Java-functions and Methods/factorialOfNumber.java
@@ -3,9 +3,9 @@ class D {
 	public static void printfactorial (int n) {
         //for-loop
         int Factorial = 1;
-				if (n < 0) {
-					System.out.println("Invaild Number");
-				}
+		if (n < 0) {
+		System.out.println("Invaild Number");
+		}
         for (int i = n; i>=1; i--) {
         Factorial = Factorial*i;
         }

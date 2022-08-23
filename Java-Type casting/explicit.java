81e56980cc96eb33e28d61ec5b3bb4a9133d768c
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-08-23 16:51:08 -0700

    Java

---
+++ Java-Type casting/explicit.java
@@ -0,0 +1,10 @@
+class explicit {
+	public static void main(String[] args) {
+		double a = 0.14;
+		char b = (char)a;
+		System.out.println(b);
+		long c = 21111;
+		int d = (int)c;
+		System.out.println(d);
+	}
+}
---
+++ Java-Type casting/implicit.java
@@ -0,0 +1,10 @@
+class implicit {
+	public static void main(String[] args) {
+		short a = 3;
+		float b = a;
+		System.out.println(b);
+		int c = 27;
+		double d = c;
+		System.out.println(d);
+	}
+}
